64ca0f0f326ce8043d5ad9cb09c33e9636e8ebdb
danin342 <542475@edu.rocmn.nl>
2022-09-05 15:06:42 +0200

    commit

---
+++ excercise_index.py
@@ -0,0 +1,7 @@
+alphabet = 'abcdefgh'
+
+print(alphabet[0])
+print(alphabet[2])
+print(alphabet[-1])
+print(alphabet[-3])
+
---
+++ excercise_variable_and_in.py
@@ -0,0 +1,10 @@
+s1 = 'good'
+s2 = 'bad'
+s3 = 'silly'
+
+print('a = ' + str(('ll' in s3)))
+print('b = ' + str((' ' not in s1)))
+print('c = ' + str(s1 + ' ' + s2 + ' ' + s3))
+print('d = ' + str(' ' in (s1 + s2 + s3)))
+print('e = ' + str(5*s3))
+print('f = ' + str(len(s1+s2+s3)))
